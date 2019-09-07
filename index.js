b24f4fb4540dec8d1ed7bf398ebe6ab8eb7fce3c
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-09-07 09:38:30 -1000

    finished 2nd option, ready for review

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+console.time("timer");
+
 function locate(array, target) {
-  let string = array.join(" ");
-  return string.includes(target);
+  // let string = array.join(" ");
+  // return string.includes(target); //console.time()...0.020ms
+
+  isLocated = false;
+
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      locate(array[i], target);
+    } else if (array[i] === target) {
+      isLocated = true;
+    }
+  }
+
+  return isLocated; //console.time()...0.161ms
 }
 
+console.timeEnd("timer");
 module.exports = locate;
